7382928d7dd12ee591cbf6f1cafb1b67d8e77d10
bunda3d <bunda3@yahoo.com>
2021-03-17 04:43:57 -0500

    Add shopping cart "basket" functionality, tested in swagger

---
+++ Microservices/src/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using Basket.API.Repositories.Interfaces;
+using Basket.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+	[Route("api/vi/[controller]")]
+	[ApiController]
+	public class BasketController : ControllerBase
+	{
+		private readonly IBasketRepository _repository;
+
+		public BasketController(IBasketRepository repository)
+		{
+			_repository = repository ?? throw new ArgumentNullException(nameof(repository));
+		}
+
+		[HttpGet]
+		[ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<BasketCart>> GetBasket(string userName)
+		{
+			var basket = await _repository.GetBasket(userName);
+			//null check for empty cart, if so => return new empty cart in user's name
+			return Ok(basket ?? new BasketCart(userName));
+		}
+
+		[HttpPost]
+		[ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<BasketCart>> UpdateBasket([FromBody] BasketCart basket)
+		{
+			return Ok(await _repository.UpdateBasket(basket));
+		}
+
+		[HttpDelete("{userName}")]
+		[ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult> DeleteBasket(string userName)
+		{
+			return Ok(await _repository.DeleteBasket(userName));
+		}
+
+	}
+}
--- Microservices/src/Basket/Basket.API/Startup.cs
+++ Microservices/src/Basket/Basket.API/Startup.cs
@@ -1,3 +1,7 @@
+using Basket.API.Data;
+using Basket.API.Data.Interfaces;
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,12 +29,21 @@ namespace Basket.API
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
-		{
+		{ //singleton because creating one connection w/ Redis db
+			services.AddSingleton<ConnectionMultiplexer>(sp =>
+			{
+				var configuration = ConfigurationOptions.Parse(Configuration.GetConnectionString("Redis"), true);
+				return ConnectionMultiplexer.Connect(configuration);
+			});
+
+			// add transient because want to create services each time creation of these objects is requested.
+			services.AddTransient<IBasketContext, BasketContext>();
+			services.AddTransient<IBasketRepository, BasketRepository>();
 
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
 			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket API", Version = "v1" });
 			});
 		}
 
@@ -40,7 +54,7 @@ namespace Basket.API
 			{
 				app.UseDeveloperExceptionPage();
 				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket API v1"));
 			}
 
 			app.UseRouting();
